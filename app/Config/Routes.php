591d500e867f72aaf18e575ac7da5f1168ab1329
jhoannad <144778227+jhoannad@users.noreply.github.com>
2023-09-24 20:48:29 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/test', 'HomeControl::test');
+$routes->get('/song/search', 'HomeControl::search');
