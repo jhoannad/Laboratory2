1a133b2146361c944a4204ec274b15c70e9bd67c
jhoannad <144778227+jhoannad@users.noreply.github.com>
2023-10-09 00:55:34 +0800

    main

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'lab2',
+        'database'     => 'llab2',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/test', 'HomeControl::test');
-$routes->get('/song/search', 'HomeControl::search');
+$routes->get('/main', 'MainController::index');
+$routes->get('/search', 'MainController::search');
+$routes->post('/songupload', 'MainController::songupload');
+$routes->post('/makeplaylist', 'MainController::makeplaylist');
+$routes->get('/delplaylist/(:num)', 'MainController::delplaylist/$1');
+$routes->get('/selplaylist/(:any)', 'MainController::selplaylist/$1');
+$routes->get('/addmusictoplaylist/(:num)', 'MainController::addmusictoplaylist/$1');
+$routes->get('/removemusicfromplaylist/(:num)', 'MainController::removemusicfromplaylist/$1');
